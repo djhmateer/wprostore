01e11ac2fa95a10d205f6d1ab4aaea02b8106ed8
Dave Mateer <davemateer@gmail.com>
2025-03-24 14:37:30 +0000

    auto

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -26,12 +26,15 @@ export const getProductBySlug = async (slug: string) => {
   const startTime = performance.now();
 
   // Enable query logging for this specific query
-  await prisma.$queryRaw`SET log_statement = 'all'`;
+  // am doing in the wrpapper function now. db/prisma.ts
+  // get an Raw query failed. Code: `42501`. Message: `ERROR: permission denied to set parameter "log_statement"`
+  // on live
+
+  // await prisma.$queryRaw`SET log_statement = 'all'`;
 
   console.log("getProductBySlug", slug);
   const foo = await prisma.product.findFirst({
-    where: { slug: slug }
-
+    where: { slug: slug },
   });
 
   const endTime = performance.now();
